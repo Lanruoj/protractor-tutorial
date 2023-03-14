a7f05d91cda1c0ba1335d311f34c20cf21eee14b
Tane Kaio <tanesamuelkaio@gmail.com>
2023-03-15 10:48:39 +1100

    Installed plugins

--- tests/calculator.js
+++ tests/calculator.js
@@ -1,10 +1,17 @@
-const homepage = require("../pages/homepage")
+const homepage = require("../pages/homepage");
 describe("Demo calculator tests", () => {
-    it("Adds 1 plus 4 to return 5", () => {
-        homepage.get("http://juliemr.github.io/protractor-demo/")
-        homepage.enterFirstNumber("1")
-        homepage.enterSecondNumber("4")
-        homepage.clickGo()
-        homepage.verifyResult("5")
-    })
-})
+  it("Adds 1 plus 4 to return 5", () => {
+    homepage.get("http://juliemr.github.io/protractor-demo/");
+    homepage.enterFirstNumber("1");
+    homepage.enterSecondNumber("4");
+    homepage.clickGo();
+    homepage.verifyResult("5");
+  });
+  it("Adds 1 plus 4 to return 5", () => {
+    homepage.get("http://juliemr.github.io/protractor-demo/");
+    homepage.enterFirstNumber("1");
+    homepage.enterSecondNumber("4");
+    homepage.clickGo1();
+    homepage.verifyResult("5");
+  });
+});
